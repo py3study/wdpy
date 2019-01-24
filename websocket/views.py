ac6d4928f122a495cf00962554a683e2cd184670
xiang.xiao <xiang.xiao@smartriver.com.cn>
2019-01-24 12:19:28 +0800

    修改停止条件

--- websocket/views.py
+++ websocket/views.py
@@ -15,7 +15,7 @@ import paramiko
 #     stdin, stdout, stderr = ssh.exec_command(comm,get_pty=True)
 #     result = stdout.read()
 #     ssh.close()
-#     return result
+#     yield result
 
 
 @accept_websocket
@@ -28,11 +28,13 @@ def echo_once(request):
             return render(request, 'index.html')
     else:
         for message in request.websocket:
-            message = message.decode('utf-8')
+            message = message.decode('utf-8')  # 接收前端发来的数据
+            print(message)
             if message == 'backup_all':#这里根据web页面获取的值进行对应的操作
                 command = 'bash /opt/test.sh'#这里是要执行的命令或者脚本
 
-                hostname = '192.168.91.128'
+                # 远程连接服务器
+                hostname = '192.168.0.162'
                 username = 'root'
                 password = 'root'
 
@@ -41,11 +43,14 @@ def echo_once(request):
                 ssh.connect(hostname=hostname, username=username, password=password)
                 # 务必要加上get_pty=True,否则执行命令会没有权限
                 stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+                # result = stdout.read()
+                # 循环发送消息给前端页面
                 while True:
-                    nextline = stdout.readline()
-                    request.websocket.send((nextline.strip()).encode('utf-8')) # 发送消息到客户端
+                    nextline = stdout.readline().strip()  # 读取脚本输出内容
+                    # print(nextline.strip())
+                    request.websocket.send(nextline) # 发送消息到客户端
                     # 判断消息为空时,退出循环
-                    if nextline == "" and nextline != None:
+                    if not nextline:
                         break
 
                 ssh.close()  # 关闭ssh连接
